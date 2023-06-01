5fcb33614a8cf29d8e9f4b01b96cc88f6ff4de74
Lesia Humenna <lgumenna61@gmail.com>
2023-06-01 14:18:18 +0200

    addes minute and second

--- script.js/task.js
+++ script.js/task.js
@@ -1,3 +1,4 @@
-const user = prompt("Enter the number!");
-let num = user * 60;
-alert(`${num} sec in -${user} hours`);
+const user = prompt("Enter the number on hours!");
+let minute = user * 60;
+let second = user * 3600;
+alert(`${minute} minuts in -${user} hours and ${second} second`);
